c624d6836514186b4c58d575df611b5ea0195849
nillynill <114229285+nillynill@users.noreply.github.com>
2025-05-30 10:12:11 -0700

    Create a new function to get Computer's Choice in the game

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,1 +1,24 @@
-console.log("Code Of Rock-Paper-Scissors Game in Javascript")
+console.log("Code Of Rock-Paper-Scissors Game in Javascript");
+
+function getComputerChoice(){
+
+    let random = Math.random();
+        console.log (random);
+
+    let computerChoice;
+
+        if (random<=0.33){
+            computerChoice = "ROCK";
+        }
+        else if (random>0.33 && random <=0.66){
+            computerChoice = "PAPER";
+        }
+        else{
+            computerChoice = "SCISSORS";
+        }
+        
+        console.log(computerChoice);
+        return computerChoice;
+}
+
+getComputerChoice();
