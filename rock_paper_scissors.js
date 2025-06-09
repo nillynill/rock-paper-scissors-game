8588c959fbf33ed242afc05c8b9d13512d87f93d
nillynill <114229285+nillynill@users.noreply.github.com>
2025-06-09 04:06:31 -0700

    Change variable names to camel case style to abide by general sofware community customs

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,80 +1,80 @@
 console.log("WELCOME TO ROCK-PAPER-SCISSORS GAME IN JAVASCRIPT CONSOLE");
 
-let HUMAN_SCORE = 0;
-let COMPUTER_SCORE = 0;
+let humanScore = 0;
+let computerScore = 0;
 
   
   //Create a function for prompting human player's choice.
   
 function getHumanChoice() {
 
-    const HUMAN_CHOICE = prompt("What is your choice? ROCK, PAPER or SCISSORS?").toUpperCase();
-    console.log (HUMAN_CHOICE);
-    return HUMAN_CHOICE;
+    const humanChoice = prompt("What is your choice? ROCK, PAPER or SCISSORS?").toUpperCase();
+    console.log (humanChoice);
+    return humanChoice;
 }
 
 //Create a function for randomly assigning ROCK, PAPER or SCISSORS to the computer(CPU).
 
 function getComputerChoice() {
-  let RANDOM = Math.random();
-  console.log (RANDOM);
+  let random = Math.random();
+  console.log (random);
 
-let COMPUTER_CHOICE;
+let computerChoice;
 
-  if (RANDOM<=0.33){
-      COMPUTER_CHOICE = "ROCK";
+  if (random<=0.33){
+      computerChoice = "ROCK";
   }
-  else if (RANDOM>0.33 && RANDOM <=0.66){
-      COMPUTER_CHOICE = "PAPER";
+  else if (random>0.33 && random <=0.66){
+      computerChoice = "PAPER";
   }
   else{
-      COMPUTER_CHOICE = "SCISSORS";
+      computerChoice = "SCISSORS";
   }
   
-  console.log(COMPUTER_CHOICE);
-  return COMPUTER_CHOICE;
+  console.log(computerChoice);
+  return computerChoice;
   }
   
   
 
-  function playGame(){
+  function playGame(){ //start game, play 5 rounds and declare scores
   
-     const NUMBER_OF_ROUNDS = 5
-     for (let i = 1; i <= NUMBER_OF_ROUNDS; i++) {
+     const numberOfRounds = 5
+     for (let i = 1; i <= numberOfRounds; i++) {
       console.log(`ROUND NUMBER ${i}`);
     
 
-    function playRound(HUMAN_CHOICE, COMPUTER_CHOICE) { //Create a function for simulating one round of play.
-      if (COMPUTER_CHOICE === "ROCK" && HUMAN_CHOICE === "PAPER" ||
-        COMPUTER_CHOICE === "PAPER" && HUMAN_CHOICE === "SCISSORS" ||
-        COMPUTER_CHOICE === "SCISSORS" && HUMAN_CHOICE === "ROCK") { // Win Conditions
-            HUMAN_SCORE++;
-            console.log(`You won this round! Your ${HUMAN_CHOICE} beats CPU's ${COMPUTER_CHOICE}.`);
+    function playRound(humanChoice, computerChoice) { //Create a function for simulating one round of play.
+      if (computerChoice === "ROCK" && humanChoice === "PAPER" ||
+        computerChoice === "PAPER" && humanChoice === "SCISSORS" ||
+        computerChoice === "SCISSORS" && humanChoice === "ROCK") { // Win Conditions
+            humanScore++;
+            console.log(`You won this round! Your ${humanChoice} beats CPU's ${computerChoice}.`);
 
-      } else if (COMPUTER_CHOICE === HUMAN_CHOICE) { // Tie Condition
-            console.log(`You tied this round! You and the CPU chose ${HUMAN_CHOICE}.`);
+      } else if (computerChoice === humanChoice) { // Tie Condition
+            console.log(`You tied this round! You and the CPU chose ${humanChoice}.`);
 
       } else if (
-        COMPUTER_CHOICE === "PAPER" && HUMAN_CHOICE === "ROCK" ||
-        COMPUTER_CHOICE === "SCISSORS" && HUMAN_CHOICE === "PAPER" ||
-        COMPUTER_CHOICE === "ROCK" && HUMAN_CHOICE === "SCISSORS"){ // Lose Conditions
-            COMPUTER_SCORE++;
-            console.log(`You lost this round! CPU's ${COMPUTER_CHOICE} beats your ${HUMAN_CHOICE}.`);
+        computerChoice === "PAPER" && humanChoice === "ROCK" ||
+        computerChoice === "SCISSORS" && humanChoice === "PAPER" ||
+        computerChoice === "ROCK" && humanChoice === "SCISSORS"){ // Lose Conditions
+            computerScore++;
+            console.log(`You lost this round! CPU's ${computerChoice} beats your ${humanChoice}.`);
 
       } else { // Invalid Entry Conditions (for type errors or empty input)
-            COMPUTER_SCORE++;
+            computerScore++;
             console.log(`Invalid entry! You lost this round!.`);
       }
   
     }
   
-      const HUMAN_CHOICE = getHumanChoice();
-      const COMPUTER_CHOICE = getComputerChoice();
-      playRound(HUMAN_CHOICE, COMPUTER_CHOICE);
+      const humanChoice = getHumanChoice();
+      const computerChoice = getComputerChoice();
+      playRound(humanChoice, computerChoice);
 
       console.log("CURRENT SCORE:");
-      console.log(`YOU: ${HUMAN_SCORE}`);
-      console.log(`CPU: ${COMPUTER_SCORE}`);
+      console.log(`YOU: ${humanScore}`);
+      console.log(`CPU: ${computerScore}`);
   }
 }
 
@@ -82,5 +82,5 @@ let COMPUTER_CHOICE;
 
 
   console.log("FINAL SCORE:");
-  console.log(`YOU: ${HUMAN_SCORE}`);
-  console.log(`CPU: ${COMPUTER_SCORE}`);
+  console.log(`YOU: ${humanScore}`);
+  console.log(`CPU: ${computerScore}`);
